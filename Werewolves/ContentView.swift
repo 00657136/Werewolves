fd215fb44fca90acab79aa2002b08613cdb15b77
user21 <osbert5618841@gmail.com>
2019-12-25 15:28:10 +0800

    Initial Commit

---
+++ Werewolves/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Werewolves
+//
+//  Created by User21 on 2019/12/25.
+//  Copyright © 2019 Claudio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
